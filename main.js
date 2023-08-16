2a6493be4660cb3c3dd18e97584f4c3bb848e397
Bogdan B <bodobogdan57@gmail.com>
2023-08-16 15:34:50 +0300

    del-and-edit-orders

--- main.js
+++ main.js
@@ -58,6 +58,8 @@ function setupPopstateEvent() {
   });
 }
 
+
+
 function setupInitialPage() {
   const origin = window.location.origin;
 
@@ -76,7 +78,7 @@ const createEvent = (eventData, userId) => {
   const contentMarkup = `
     <div class="event">
       <h4 class="event-title">${eventData.eventName}</h4>
-      <a href="main.php?eventName=${encodeURIComponent(eventData.eventId)}&userId=${userId}">
+      <a href="main.php?eventId=${encodeURIComponent(eventData.eventId)}&userId=${userId}">
         <img src="images/${eventData.eventName}.jpg" alt="Event Image" class="event-image" />
       </a>
     </div>
@@ -121,49 +123,175 @@ function renderEventPage(userId, eventName) {
 
 
 async function renderOrdersPage() {
-  const mainContentDiv = document.querySelector('.main-content-component-orders');
-  mainContentDiv.innerHTML = getOrdersPageTemplate();
+  const mainContentDiv = document.querySelector('.main-content-component');
+  if (mainContentDiv) {
+    mainContentDiv.innerHTML = getOrdersPageTemplate();
 
-  const response = await fetch('http://localhost:8080/api/Order/Orders');
-  const data = await response.json();
-  const table = createOrdersTable(data);
-  const mainDiv = document.querySelector('.orders');
-  mainDiv.appendChild(table);
+    const response = await fetch('http://localhost:8080/api/Order/Orders');
+    const data = await response.json();
+    const table = createOrdersTable(data);
+    const mainDiv = document.querySelector('.orders');
+    if (mainDiv) {
+      mainDiv.appendChild(table);
+    }
+    disableUpdateFields(data);
+    setupTableButtonEvents(data);
+  }
 }
 
+function disableUpdateFields(data){
+  const userId = localStorage.getItem("userId");
+  const userOrders = data.filter(order => order.user.userId === parseInt(userId, 10));
+  userOrders.forEach((order)=>{
+    const ticketsinput = document.querySelector("#numberoftickets-"+order.orderId);
+    ticketsinput.disabled=true;
+  })
+}
+
+function setupTableButtonEvents(orders){
+  const userId = localStorage.getItem("userId");
+  const userOrders = orders.filter(order => order.user.userId === parseInt(userId, 10));
+
+  userOrders.forEach((order) => {
+    const ticketsinput = document.querySelector("#numberoftickets-"+order.orderId);
+    
+    const deleteButton = document.querySelector('#deleteButton-' +order.orderId);
+    deleteButton.addEventListener('click', () => {
+      handleDeleteOrder(order);
+    });
+
+    // Attach an event listener to the "Edit" button
+    const editButton = document.querySelector('#editButton-' +order.orderId);
+    editButton.addEventListener('click', () => {
+      if(ticketsinput.disabled === true){
+        ticketsinput.disabled = false;
+      }
+      else{
+        ticketsinput.disabled=true;
+        handleEditOrder(order);
+      }
+      
+    });
+  })
+}
+
+
 function createOrdersTable(orders) {
+  const userId = localStorage.getItem("userId");
+  const userOrders = orders.filter(order => order.user.userId === parseInt(userId, 10));
+
   const table = document.createElement('table');
   table.classList.add('orders-table');
 
   // Create table header row
   const headerRow = document.createElement('tr');
   headerRow.innerHTML = `
-      <th>Event Name</th>
-      <th>Event Type</th>
-      <th>Number of Tickets</th>
-      <th>Price per Ticket</th>
-      <th>Total Price</th>
+    <th class="table-header">Event Name</th>
+    <th class="table-header">Ticket Type</th>
+    <th class="table-header">Number of Tickets</th>
+    <th class="table-header">Price per Ticket</th>
+    <th class="table-header">Total Price</th>
+    <th class="table-header">Edit|Delete</th>
   `;
   table.appendChild(headerRow);
 
   // Create table rows for each order
-  orders.forEach((order) => {
-      const orderRow = document.createElement('tr');
-      orderRow.classList.add('order');
+  userOrders.forEach((order) => {
+    const orderRow = document.createElement('tr');
+    orderRow.classList.add('order');
 
-      orderRow.innerHTML = `
-          <td>${order.ticketCategory.event.eventName}</td>
-          <td contenteditable="true">${order.ticketCategory.event.eventType.eventtypeName}</td>
-          <td contenteditable="true">${order.numberOfTickets}</td>
-          <td>$${order.ticketCategory.price}</td>
-          <td>$${order.totalPrice}</td>
-      `;
+    orderRow.innerHTML = `
+      <td>${order.ticketCategory.event.eventName}</td>
+      <td>${order.ticketCategory.description}</td>
+      <td><input class ="numberOfTicketsInput" id ="numberoftickets-${order.orderId}" value = "${order.numberOfTickets}" type =number min="1"></input></td>
+      <td>$${order.ticketCategory.price}</td>
+      <td>$${order.totalPrice}</td>
+      <td>
+      <button class="edit-button" id="editButton-${order.orderId}"><img src="./src/assets/edit.jpg" alt="Edit|Delete" /></button>
+      <button class="delete-button" id="deleteButton-${order.orderId}"><img src="./src/assets/delete.jpg" alt="Edit|Delete" /></button>
+      </td>
+    `;
 
-      table.appendChild(orderRow);
+    
+
+    table.appendChild(orderRow);
   });
 
   return table;
 }
+
+async function handleDeleteOrder(order) {
+  const response = await fetch(`http://localhost:8080/api/Order/${order.orderId}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (response.ok) {
+    // Remove the corresponding row from the table
+    const deleteRowIndex = findDeleteRowIndex(order.orderId);
+    if (deleteRowIndex !== -1) {
+      const table = document.querySelector('.orders-table');
+      table.deleteRow(deleteRowIndex);
+      
+    }
+    alert("Order deleted.Please reload the page.")
+  } else {
+    console.error('Failed to delete order:', order);
+  }
+}
+
+async function handleEditOrder(order) { 
+  const numberOfTickets= document.querySelector('#numberoftickets-' + order.orderId);
+  const numberOfTicketsInput = parseInt(numberOfTickets.value, 10); // Parse the 
+  console.log(order.user);
+  console.log(order.ticketCategory);
+  console.log(order.orderAt);
+  console.log(numberOfTicketsInput.value);
+  console.log(order.ticketCategory.price*numberOfTicketsInput)
+  const response = await fetch(`http://localhost:8080/api/Order/${order.orderId}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      user: order.user,
+      ticketCategory:order.ticketCategory,
+      orderAt:order.orderAt,
+      numberOfTickets:numberOfTicketsInput,
+      totalPrice:order.ticketCategory.price*numberOfTicketsInput,
+      orderId:order.orderId
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (response.ok) {
+    // Remove the corresponding row from the table
+    const editRowIndex = findDeleteRowIndex(order.orderId);
+    if (editRowIndex !== -1) {
+      const table = document.querySelector('.orders-table');
+      table.deleteRow(editRowIndex);
+      
+    }
+    alert("Order Edited.Please reload the page.")
+  } else {
+    console.error('Failed to edit order:', order);
+  }
+}
+
+function findDeleteRowIndex(orderId) {
+  const table = document.querySelector('.orders-table');
+  const rows = table.querySelectorAll('.order');
+  for (let i = 0; i < rows.length; i++) {
+    const rowOrderId = parseInt(rows[i].getAttribute('data-order-id'));
+    if (rowOrderId === orderId) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+
 
 
 // Render content based on URL
@@ -178,31 +306,41 @@ function renderContent(url) {
 
   if (url === '/' || page === 'index.html') {
     renderHomePage();
-  } else if (page === 'orders.html') {
+  } else if (url === '/'||page === 'orders.html') {
     renderOrdersPage();
   } else if (url.startsWith('/main.php')) {
     renderEventPage(userId, urlParams.get('eventName'));
   }
 }
 
+function searchEvents(query, events) {
+  return events.filter(event => {
+    const eventName = event.eventName.toLowerCase();
+    return eventName.includes(query.toLowerCase());
+  });
+}
 
-$(document).on("keyup", "#search_event_input", function() {
-  var param = $(this).val();
-  param = param.toLowerCase();
 
-  if (param != '') {
-      $("#main_container_div").empty();
-      var container = "";
-      var eventCounter = 0;
-      for (var key in arr) {
-          if (arr[key].includes(param)) {
-              ++eventCounter;
-              let copyString = copyArr[key];
-              container += '<a href="${eventData.eventName}">' + copyArr[key] + '"><div class="each_card_event_main_container"><img src="images/${eventData.eventName}.jpg" alt="Event Image" class="event-image"/>' + copyArr[key] + 'class="each_event_picture" /><div class="inner_each_event_name_container"><p class="each_event_name">' + copyString + '</div></div></a>';
-          }}
-        
-        
-        }})
+document.addEventListener('DOMContentLoaded', () => {
+  const searchInput = document.getElementById('search_event_input');
+
+  searchInput.addEventListener('keyup', async () => {
+    const response = await fetch("http://localhost:8080/api/Event/Events");
+    const data = await response.json();
+
+    const searchTerm = searchInput.value.trim();
+    const filteredEvents = searchEvents(searchTerm, data);
+
+    const mainDiv = document.querySelector('.events');
+    mainDiv.innerHTML = '';
+
+    filteredEvents.forEach((event) => {
+      mainDiv.appendChild(createEvent(event));
+    });
+  });
+});
+
+
 
 document.addEventListener("scroll", function(e) {
   if ($("body").scrollTop() > 600) {
